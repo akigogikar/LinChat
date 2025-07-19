cbd86462fadd5b973b04d8ef4cb9d4f65b2feff6
akigogikar <agogikar@linbeck.com>
2025-07-19 16:06:45 -0500

    Add sidebar layout with workspace selector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,24 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import { AppBar, Toolbar, Button } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Documents from './pages/Documents.jsx'
 import ChatPage from './pages/Chat.jsx'
 import Login from './Login.jsx'
 import Register from './Register.jsx'
 import AdminDashboard from './admin/AdminDashboard.jsx'
+import AppLayout from './components/AppLayout.jsx'
 import './App.css'
 
 export default function App() {
-  const token = localStorage.getItem('token')
-
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/documents">Documents</Button>
-          <Button color="inherit" component={Link} to="/chat">Chat</Button>
-          {token && <Button color="inherit" component={Link} to="/admin">Admin</Button>}
-          <Button color="inherit" component={Link} to="/login">Login</Button>
-          <Button color="inherit" component={Link} to="/register">Register</Button>
-        </Toolbar>
-      </AppBar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/documents" element={<Documents />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<AppLayout />}> 
+        <Route index element={<Home />} />
+        <Route path="documents" element={<Documents />} />
+        <Route path="chat" element={<ChatPage />} />
+        <Route path="admin" element={<AdminDashboard />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    </Routes>
   )
 }
